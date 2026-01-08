3c2133aecc08321f849460a2dc4ba872e91ea440
Daniel Zellmer <daniel.zellmer@esker.com>
2026-01-08 15:34:05 -0600

    Update pdfsizeopt.py to accept Python 3

--- pdfsizeopt.py
+++ pdfsizeopt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python3.10
 
 """:" # pdfsizeopt: PDF file size optimizer
 
@@ -7,14 +7,14 @@ test "$P" && test "${P#/}" = "$P" && P="${0%/*}/$P"
 test "$P" || P="$0"
 Q="${P%/*}"/pdfsizeopt_libexec/python
 test -f "$Q" && exec "$Q" -E -- "$P" ${1+"$@"}
-type -p python2.7 >/dev/null 2>&1 && exec python2.7 -- "$P" ${1+"$@"}
-type -p python2.6 >/dev/null 2>&1 && exec python2.6 -- "$P" ${1+"$@"}
-type -p python2.5 >/dev/null 2>&1 && exec python2.5 -- "$P" ${1+"$@"}
-type -p python2.4 >/dev/null 2>&1 && exec python2.4 -- "$P" ${1+"$@"}
+type -p python3.13 >/dev/null 2>&1 && exec python3.13 -- "$P" ${1+"$@"}
+type -p python3.12 >/dev/null 2>&1 && exec python3.12 -- "$P" ${1+"$@"}
+type -p python3.11 >/dev/null 2>&1 && exec python3.11 -- "$P" ${1+"$@"}
+type -p python3.10 >/dev/null 2>&1 && exec python3.10 -- "$P" ${1+"$@"}
 exec python -- "$P" ${1+"$@"}; exit 1
 
-This is a Python 2.x script, it works with Python 2.4, 2.5, 2.6 and 2.7. It
-doesn't work with Python 3.x. Feel free to replace the #! line with
+This is a Python 3.10+ script, it works with Python 3.10 or newer. It
+doesn't work with Python 2.x. Feel free to replace the #! line with
 `#! /usr/bin/python', `#! /usr/bin/env python' or whatever suits you best.
 """
 
@@ -22,9 +22,8 @@ import os
 import os.path
 import sys
 
-if not ((2, 4) <= sys.version_info[:2] < (3, 0)):
-  sys.stderr.write(
-      'fatal: Python version 2.4, 2.5, 2.6 or 2.7 needed for: %s\n' % __file__)
+if sys.version_info[:2] < (3, 10):
+  sys.stderr.write('fatal: Python version 3.10+ needed for: %s\n' % __file__)
   sys.exit(1)
 
 script_dir = os.path.dirname(__file__)
@@ -33,8 +32,7 @@ try:
   script_dir = os.path.dirname(__file__)
 except (OSError, AttributeError, NotImplementedError):
   pass
-if os.path.isfile(os.path.join(
-    script_dir, 'lib', 'pdfsizeopt', 'main.py')):
+if os.path.isfile(os.path.join(script_dir, 'lib', 'pdfsizeopt', 'main.py')):
   sys.path[0] = os.path.join(script_dir, 'lib')
 
 import lib
