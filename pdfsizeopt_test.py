d9c526111b6027351548b2690bfd2547f57a9c99
Daniel Zellmer <daniel.zellmer@esker.com>
2026-02-04 14:01:24 -0600

    Python 3 update - testFindEqclassesTwoGroupsByHead

--- pdfsizeopt_test.py
+++ pdfsizeopt_test.py
@@ -1194,17 +1194,17 @@ class PdfSizeOptTest(unittest.TestCase):
 
   def testFindEqclassesTwoGroupsByHead(self):
     pdf = main.PdfData()
-    pdf.trailer = main.PdfObj('0 0 obj<<>>endobj')
-    pdf.objs[5] = main.PdfObj('0 0 obj<</S(q)/P 6 0 R>>endobj')
-    pdf.objs[6] = main.PdfObj('0 0 obj<</S(q)/Q 5 0 R >>endobj')
-    pdf.objs[3] = main.PdfObj('0 0 obj<</S(q)/P 4 0 R  >>endobj')
-    pdf.objs[4] = main.PdfObj('0 0 obj<</S(q)/Q 3 0 R   >>endobj')
+    pdf.trailer = main.PdfObj(b'0 0 obj<<>>endobj')
+    pdf.objs[5] = main.PdfObj(b'0 0 obj<</S(q)/P 6 0 R>>endobj')
+    pdf.objs[6] = main.PdfObj(b'0 0 obj<</S(q)/Q 5 0 R >>endobj')
+    pdf.objs[3] = main.PdfObj(b'0 0 obj<</S(q)/P 4 0 R  >>endobj')
+    pdf.objs[4] = main.PdfObj(b'0 0 obj<</S(q)/Q 3 0 R   >>endobj')
     new_objs = main.PdfData.FindEqclasses(pdf.objs)
     for obj_num in new_objs:
       new_objs[obj_num] = (new_objs[obj_num].head, new_objs[obj_num].stream)
     self.assertEqual(
-        {3: ('<</S(q)/P 4 0 R>>', None),
-         4: ('<</S(q)/Q 3 0 R>>', None)}, new_objs)
+        {3: (b'<</S(q)/P 4 0 R>>', None),
+         4: (b'<</S(q)/Q 3 0 R>>', None)}, new_objs)
 
   def testFindEqclassesTwoGroupsWithTrailer(self):
     pdf = main.PdfData()
